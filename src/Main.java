de1c97a496a6a721f0bb349bd66c4e4fa594e981
MrKOT92 <mrkot9292@gmail.com>
2023-04-28 15:48:21 +0200

    the code has been simplified

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,38 @@ public class Main {
     DECEMBER,
   }
 
+  private enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN,
+  }
 
+  private static Season getSeason(Month month) {
+    return switch (month) {
+      case DECEMBER, JANUARY, FEBRUARY -> Season.WINTER;
+      case MARCH, APRIL, MAY -> Season.SPRING;
+      case JUNE, JULY, AUGUST -> Season.SUMMER;
+      case SEPTEMBER, OCTOBER, NOVEMBER -> Season.AUTUMN;
+    };
+  }
+  private static Season getSeasonFromMap(Month month) {
+    Map<Month, Season> seasonPerMonth = new HashMap<>();
+    seasonPerMonth.put(Month.JANUARY, Season.WINTER);
+    seasonPerMonth.put(Month.FEBRUARY, Season.WINTER);
+    seasonPerMonth.put(Month.MARCH, Season.SPRING);
+    seasonPerMonth.put(Month.APRIL, Season.SPRING);
+    seasonPerMonth.put(Month.MAY, Season.SPRING);
+    seasonPerMonth.put(Month.JUNE, Season.SUMMER);
+    seasonPerMonth.put(Month.JULY, Season.SUMMER);
+    seasonPerMonth.put(Month.AUGUST, Season.SUMMER);
+    seasonPerMonth.put(Month.SEPTEMBER, Season.AUTUMN);
+    seasonPerMonth.put(Month.OCTOBER, Season.AUTUMN);
+    seasonPerMonth.put(Month.NOVEMBER, Season.AUTUMN);
+    seasonPerMonth.put(Month.DECEMBER, Season.WINTER);
+
+    return seasonPerMonth.get(month);
+  }
   public static void main(String[] args) {
 
     Map<Month, Integer> daysPerMonth = new HashMap<>();
@@ -52,27 +83,9 @@ public class Main {
     System.out.print("Enter the month: ");
     String month = scanner.next();
     Month monthKey = Month.valueOf(month.toUpperCase());
-    System.out.println(month + " has " + daysPerMonth.get(monthKey) + " days");
-
-    int nextMonthIndex = (monthKey.ordinal() + 1) % Month.values().length;
-    Month nextMonth = Month.values()[nextMonthIndex];
-    System.out.println(
-        "The next month " + nextMonth + " has " + daysPerMonth.get(nextMonth) + " days");
-    System.out.println();
-
-    System.out.print("Enter the month number: ");
-    int monthNumber = scanner.nextInt();
-    int monthIndex = monthNumber - 1;
-    monthKey = Month.values()[monthIndex];
-    System.out.println(monthKey + " has " + daysPerMonth.get(monthKey) + " days");
-    System.out.println();
-
-    System.out.println("=== Перебор через Month.values() ===");
-
-    for (Month key : Month.values()) {
-
-      System.out.println(key + ": " + daysPerMonth.get(key));
-    }
+    System.out.println(month + " has " + daysPerMonth.get(monthKey) + " days.");
+    System.out.println(month + " is a part of the " + getSeason(monthKey) + " season.");
+    System.out.println(month + " is a part of the " + getSeasonFromMap(monthKey) + " season.");
   }
 }
 
